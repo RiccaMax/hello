6182e30557fb325276cd33427602a78b8aad0b7f
RiccaMax <mricca2@huskers.unl.edu>
2025-08-28 21:15:12 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Maximus Ricca
+ *         mricca2@huskers.unl.edu
+ * Date: Thursday, August 28th
+ *
+ * Hello World program in C for Hack 1.0
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Maximus Ricca!\n");
+  printf("Majoring in CSCE!\n");
+
+  return 0;
+}
