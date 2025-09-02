ccf58a2d40834ff6e5196a1f692badf90822e43e
RiccaMax <mricca2@huskers.unl.edu>
2025-09-01 20:34:42 -0500

    Initial commit: add hello.c printing name and major

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Maximus Ricca
+ * Date: 09/01/25
+ *
+ * A simple hello world program in C for Hack 1.o
+ *
+ */
+#include <stdio.h>
+
+int main()  {
+
+    printf("Maximus Ricca!\n");
+    printf("Majoring in Computer Science.\n");
+
+    return 0;
+}
